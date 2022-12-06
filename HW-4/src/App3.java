076a791b92e98ec2389a4147552a0bffd482d539
Aeshe <syslichek@mail.ru>
2022-12-06 14:25:40 +0300

    mod HW-4

--- HW-4/src/App3.java
+++ HW-4/src/App3.java
@@ -1,5 +1,8 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+// import java.util.HashMap;
+// import java.util.Map;
 
 //Написать программу, определяющую правильность расстановки скобок в выражении.
 //a+(d*3) - истина
@@ -10,37 +13,59 @@ import java.util.Map;
 // {a+]}{(d*3)} - ложь
 public class App3 {
     public static void main(String[] args) {
-        String str="<{a}+{(d*3)}>";
+        String str="{a+]}{(d*3)}";
         System.out.println(checkCorrect(str));                 
     }
-
-    public static Map<String, Integer> makeMap(String expression) {
-        String[] mathExp=expression.split("");
-        Map<String, Integer> expSymbols = new HashMap<>(); 
-        int count = 0;     
+    public static Boolean checkCorrect(String str) {
+        String[] mathExp=str.split("");
+        String[] bracStart = {"(","[","{","<"};
+        String[] bracEnd = {")","]","}",">"};
+        ArrayList<String> symbStart = new ArrayList<>(Arrays.asList(bracStart)); 
+        ArrayList<String> symbEnd = new ArrayList<>(Arrays.asList(bracEnd)); 
+        ArrayDeque<String> brackets = new ArrayDeque<String>();
         for (String string : mathExp) {
-            if(expSymbols.containsKey(string)){
-                count = expSymbols.get(string)+1;
-                expSymbols.replace(string, count); 
+            if(symbStart.indexOf(string) != -1){  //если символ найден в списке открывающих скобок
+                brackets.addFirst(symbEnd.get(symbStart.indexOf(string))); // кладем закрывающуюся скобку
             }
-            else {
-                expSymbols.put(string, 1);
-            }
+            else if (symbEnd.indexOf(string) != -1){//если символ найден в списке закрывающих скобок
+                String elem=brackets.pollFirst(); // проверяе равен ли он скобке сверху стека, удаляем если равен и идем дальше
+                if( !string.equals(elem)){
+                    return false;
+                }                
+            }            
         }
-        return expSymbols;
+        return brackets.isEmpty();   // все ли скобки удалили из стека     
+        
     }
 
-    public static Boolean checkCorrect(String expression) {
-        Map<String, Integer> expSymbols = makeMap(expression);
-        String[] symbols = {"(", ")", "[", "]", "{", "}", "<", ">"}; 
+    // public static Map<String, Integer> makeMap(String expression) {
+    //     String[] mathExp=expression.split("");
+    //     Map<String, Integer> expSymbols = new HashMap<>(); 
+    //     int count = 0;     
+    //     for (String string : mathExp) {
+    //         if(expSymbols.containsKey(string)){
+    //             count = expSymbols.get(string)+1;
+    //             expSymbols.replace(string, count); 
+    //         }
+    //         else {
+    //             expSymbols.put(string, 1);
+    //         }
+    //     }
+    //     return expSymbols;
+    // }
+
+    // public static Boolean checkCorrect(String expression) {
+    //     Map<String, Integer> expSymbols = makeMap(expression);
+    //     String[] symbols = {"(", ")", "[", "]", "{", "}", "<", ">"}; 
         
-        for (int i = 0; i < symbols.length-1; i+=2) {
-            if(expSymbols.get(symbols[i]) != expSymbols.get(symbols[i+1])){
-                return false;
-            }
-        }
-        return true;
-    }
+    //     for (int i = 0; i < symbols.length-1; i+=2) {
+    //         if(expSymbols.get(symbols[i]) != expSymbols.get(symbols[i+1])){
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
+
     
     
 }
